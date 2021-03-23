8ab05b4fef574c346e4405569670cdd077ac4c5f
joergbuchwald <joerg.buchwald@ufz.de>
2021-03-23 13:44:54 +0100

    add python source file

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Elastic Anisotropy"""
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="ElasticAnisotropy",
+    version=0.001,
+    maintainer="Jörg Buchwald",
+    maintainer_email="joerg_buchwald@ufz.de",
+    author="Jörg Buchwald",
+    author_email="joerg.buchwald@ufz.de",
+    url="https://github.com/joergbuchwald/",
+    license="MIT -  see LICENSE.txt",
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    include_package_data=True,
+    install_requires=["numpy"],
+    py_modules=["ElasticAnisotropy"])
+
